0f1be82c6393f235e007ba2ed9486a7f83858d43
splaza28 <global-splaza28@gmail.com>
2021-12-21 00:54:05 -0600

    updated code

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server operating on ${PORT}!`);
   });
   
