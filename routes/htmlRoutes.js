4955e6472c3500649beb0f13e1ecda7aae9d4d91
splaza28 <global-splaza28@gmail.com>
2021-12-21 00:55:02 -0600

    adding routes data

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+  
+
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  module.exports = router;
