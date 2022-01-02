06d83d2eabdaf85c54bbaa70a3b88a5303fec24d
splaza28 <global-splaza28@gmail.com>
2022-01-02 03:32:03 -0600

    split up server code into api and html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,37 @@
+const { notes } = require('../data/db.json');
+const router = require('express').Router();
+const fs = require('fs');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+  });
+  
+  router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    res.json(result);
+  });
+
+  function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, '../data/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  }
+  
+
+  router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes)
+    res.json(note);
+  });
+
+  router.delete('/notes/:id', (req,res) => {
+      
+  })
+
+
+  module.exports = router;
 
--- server.js
+++ server.js
@@ -1,64 +1,18 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const app = express();
-const { notes } = require('./data/db.json');
+//const { notes } = require('./data/db.json');
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
-
-
-
-
-
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  res.json(result);
-});
-
-
-
-//function findById(id, notesArray) {
-  //const result = notesArray.file
-//}
-
-function createNewNote(body, notesArray) {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, './data/db.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
-  return note;
-}
-
-app.post('/api/notes', (req, res) => {
-  req.body.id = notes.length.toString();
-  const note = createNewNote(req.body, notes)
-  res.json(note);
-});
-
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req,res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.listen(PORT, () => {
-  console.log('API server now on port ${PORT}!');
+  console.log('API server now operating on port ${PORT}!');
 });
