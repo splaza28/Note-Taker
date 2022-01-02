b8139a4e4b43c09942ac92155987dbe0f4dd695c
splaza28 <global-splaza28@gmail.com>
2022-01-02 02:32:44 -0600

    updated files

--- server.js
+++ server.js
@@ -1,19 +1,64 @@
-const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const express = require('express');
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const { notes } = require('./data/db.json');
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
+
+
+
+
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  res.json(result);
+});
+
+
+
+//function findById(id, notesArray) {
+  //const result = notesArray.file
+//}
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './data/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+  const note = createNewNote(req.body, notes)
+  res.json(note);
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`API server operating on ${PORT}!`);
-  });
-  
+  console.log('API server now on port ${PORT}!');
+});
